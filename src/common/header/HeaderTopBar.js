1b5720d90242da0bcccef1a50a912f647f762860
Harrison Warburton <81275784+dev-harrisonw@users.noreply.github.com>
2023-01-16 22:21:11 +0000

    test

--- src/common/header/HeaderTopBar.js
+++ src/common/header/HeaderTopBar.js
@@ -9,14 +9,14 @@ const HeaderTopBar = () => {
                 <div className="row align-items-center">
                     <div className="col-lg-4 col-md-12 col-12">
                         <div className="header-left">
-                            <p><a href="#link">Get the most advanced template <FiChevronRight /></a></p>
+                            <p><a href="#link">Unleash the power of the web today<FiChevronRight /></a></p>
                         </div>
                     </div>
                     <div className="col-lg-8 col-md-12 col-12">
                         <div className="header-right">
                             <div className="address-content">
-                                <p><FiMapPin /><span>Alabama, USA</span></p>
-                                <p><FiPhone /><span><a href="#">+06 58 49 99 56</a></span></p>
+                                <p><FiMapPin /><span>Manchester, UK</span></p>
+                                <p><FiPhone /><span><a href="#">+44 7984 980188</a></span></p>
                             </div>
                             <div className="social-icon-wrapper">
                                 <ul className="social-icon social-default icon-naked">
