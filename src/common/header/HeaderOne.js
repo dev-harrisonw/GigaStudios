ae5ccbd810a554183359adc3c843650ab8e21250
Harrison Warburton <81275784+dev-harrisonw@users.noreply.github.com>
2023-01-24 00:05:55 +0000

    color change

--- src/common/header/HeaderOne.js
+++ src/common/header/HeaderOne.js
@@ -38,7 +38,7 @@ const HeaderOne = ({btnStyle, HeaderSTyle}) => {
                                     <Nav />
                                 </nav>
                                 <div className="header-btn">
-                                    <a className={`btn-default ${btnStyle}`} target="_blank" href="https://themeforest.net/checkout/from_item/33571911?license=regular">BUY NOW</a>
+                                    <a className={`btn-default ${btnStyle}`} target="_blank" href="mailto:me@harrisonwarburton.com">ENQUIRE NOW</a>
                                 </div>
                                 <div className="mobile-menu-bar ml--5 d-block d-lg-none">
                                     <div className="hamberger">
