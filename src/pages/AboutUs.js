c31629d5bcec8ff2ff0ab4922fcc863198e88574
Harrison Warburton <81275784+dev-harrisonw@users.noreply.github.com>
2023-01-29 03:30:25 +0000

    g

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -21,7 +21,7 @@ const AboutUs = () => {
                 <HeaderOne btnStyle="btn-small round btn-icon" HeaderSTyle="header-transparent" />
 
                 {/* Start Slider Area  */}
-                <div className="slider-area slider-style-1 height-850 bg_image" data-black-overlay="7" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/images/bg/bg-image-12.jpg)`}}>
+                <div className="slider-area slider-style-1 height-850 bg_image" data-black-overlay="7" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/images/about/gradient.png)`}}>
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-12">
