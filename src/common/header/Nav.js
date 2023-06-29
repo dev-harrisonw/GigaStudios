ed1274a2f348c90f8d96062edaa697f5cc638067
Harrison Warburton <81275784+dev-harrisonw@users.noreply.github.com>
2023-06-30 00:23:48 +0100

    changes

---
+++ src/common/header/Nav.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const Nav = () => {
+    return (
+        <ul className="mainmenu">
+            <li className="has-droupdown"><Link to="/">Home</Link>
+                {/* <ul className="submenu">
+                    <li><Link to="/business-consulting">Business Consulting</Link></li>
+                    <li><Link to="/business-consulting-2">Business Consulting 02</Link></li>
+                    <li><Link to="/corporate">Corporate</Link></li>
+                    <li><Link to="/business">Business</Link></li>
+                    <li><Link to="/digital-agency">Digital Agency</Link></li>
+                    <li><Link to="/finance">Finance</Link></li>
+                    <li><Link to="/company">Company</Link></li>
+                    <li><Link to="/marketing">Development Studio</Link></li>
+                    <li><Link to="/travel-agency">Travel Agency</Link></li>
+                    <li><Link to="/consulting">Consulting</Link></li>
+                    <li><Link to="/seo-agency">SEO Agency</Link></li>
+                    <li><Link to="/personal-portfolio">Personal Portfolio</Link></li>
+                    <li><Link to="/event-conference">Event Conference</Link></li>
+                    <li><Link to="/creative-portfolio">Creative Portfolio</Link></li>
+                    <li><Link to="/freelancer">Freelancer</Link></li>
+                    <li><Link to="/international-consulting">International Consulting</Link></li>
+                    <li><Link to="/startup">Startup</Link></li>
+                    <li><Link to="/web-agency">Web Agency</Link></li>
+                </ul> */}
+            </li>
+            
+            <li><Link to="/about-us">About</Link></li>
+
+            <li><Link to="/coming-soon">Blog</Link>
+                {/* <ul className="submenu">
+                    <li><Link to="/blog-grid">Blog Grid</Link></li>
+                    <li><Link to="/blog-grid-sidebar">Blog Grid Sidebar</Link></li>
+                    <li><Link to="/blog-list-view">Blog List View</Link></li>
+                    <li><Link to="/blog-list-sidebar">Blog List View Sidebar</Link></li>
+                </ul> */}
+            </li>
+
+            <li><Link to="/portfolio">Portfolio</Link>
+                {/* <ul className="submenu">
+                    <li><Link to="/portfolio">Portfolio Default</Link></li>
+                    <li><Link to="/portfolio-three-column">Portfolio Three Column</Link></li>
+                    <li><Link to="/portfolio-full-width">Portfolio Full Width</Link></li>
+                    <li><Link to="/portfolio-grid-layout">Portfolio Grid Layout</Link></li>
+                    <li><Link to="/portfolio-box-layout">Portfolio Box Layout</Link></li>
+                </ul> */}
+            </li>
+
+            <li className="has-droupdown"><Link to="/contact">Contact</Link></li>
+        </ul>
+    )
+}
+export default Nav;
---
+++ src/elements/about/AboutFour.js
@@ -0,0 +1,77 @@
+import Typed from 'react-typed';
+import React from 'react';
+import VideoTwo from '../video/VideoTwo';
+import { FiCheck } from "react-icons/fi";
+
+
+const PopupData = [
+    {
+        id: "01",
+        image: "./images/bg/bg-image-4.jpg",
+        popupLink: [
+            'https://www.youtube.com/watch?v=',
+        ],
+    }
+]
+
+const AboutFour = ({image}) => {
+    return (
+        <div className="about-area about-style-4 rn-section-gap">
+            <div className="container">
+                <div className="row row--40 align-items-center">
+                    <div className="col-lg-6">
+                        {PopupData.map((item) => (
+                            <div className="video-btn" key={item.id}>
+                                <VideoTwo imagename={`${image}`} galleryItem={item} />
+                            </div>
+                        ))}
+                    </div>
+                    <div className="col-lg-6 mt_md--40 mt_sm--40">
+                        <div className="content">
+                            <div className="inner">
+                                <h3 className="title">We specialize in <br />
+                                        <span className="title theme-gradient"><Typed
+                                            strings={[
+                                                "Web Development.",
+                                                "App Development.",
+                                                "Bespoke Development.",
+                                                "Maintenance & Support",
+                                            ]}
+                                            typeSpeed={80}
+                                            backSpeed={5}
+                                            backDelay={1000}
+                                            loop
+                                        /></span></h3>
+                                <ul className="feature-list">
+                                    <li>
+                                        <div className="icon">
+                                            <FiCheck />
+                                        </div>
+                                        <div className="title-wrapper">
+                                            <h4 className="title">Committed to Quality and Innovation</h4>
+                                            <p className="text">Our unwavering commitment to quality and innovation sets us apart, ensuring that every project we undertake exceeds expectations and delivers exceptional results.</p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="icon">
+                                            <FiCheck />
+                                        </div>
+                                        <div className="title-wrapper">
+                                            <h4 className="title">Expert Development and Design Team.</h4>
+                                            <p className="text">GigaStudios delivers unparalleled solutions with expert development and design, exceeding the unique needs of each and every client.</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                                <div className="about-btn mt--30">
+                                    <a className="btn-default" href="/">Our Story</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutFour
