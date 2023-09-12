9f5ebff6273ca89a86ad08b8252f3e52cf992f6c
Harrison Warburton <81275784+dev-harrisonw@users.noreply.github.com>
2023-09-12 03:20:02 +0100

    crawler

--- src/common/SEO.js
+++ src/common/SEO.js
@@ -8,8 +8,9 @@ const SEO = ( {title} ) => {
             <meta charSet="utf-8" />
             <title>{title} || GigaStudios - Development Studio</title>
             <meta name="robots" content="noindex, follow" />
+            <meta name="googlebot" content="noindex, follow" />
             <meta name="description" content="GigaStudios – Development Studio" />
-            <meta name="keywords" content="GigaStudios, Development, Website Development, App Development, Software Development, Software" />
+            <meta name="keywords" content="GigaStudios, Development, Website Development, App Development, Software Development, Software, Giga Studios" />
             <meta name="author" content="Harrison Warburton" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         </>
