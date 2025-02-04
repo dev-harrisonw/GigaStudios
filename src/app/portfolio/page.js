67bcd55508804cb04a863fef6c99ac6d5b81635c
Harrison Warburton <81275784+dev-harrisonw@users.noreply.github.com>
2025-02-04 02:39:52 +0000

    r

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,8 +1,6 @@
-// Remove "use client" directive
+"use Client";
 import PortfolioOne from "@/elements/portfolio/PortfolioOne";
 import SectionTitle from "@/elements/sectionTitle/SectionTitle";
-import Head from "next/head"; // Import Head for modifying the document head
-import { useEffect } from "react"; // Import useEffect for tracking page views
 import { promises as fs } from "fs";
 
 export const metadata = {
@@ -20,54 +18,29 @@ const PortfolioPage = async () => {
     "utf8"
   );
   const data = JSON.parse(file);
-
-  useEffect(() => {
-    // Tracking page view for Google Analytics
-    if (window.gtag) {
-      window.gtag('config', 'G-RQ96RJ4M5S', {
-        page_path: '/portfolio', // Update this to the current page path
-      });
-    }
-  }, []);
-
+  // console.log(data);
   return (
-    <>
-      <Head>
-        {/* Google Tag Manager script */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RQ96RJ4M5S"></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-RQ96RJ4M5S', {
-              page_path: '/portfolio', // Ensure this matches the page path
-            });
-          `}
-        </script>
-      </Head>
-      <div className="main-content">
-        <div className="rwt-portfolio-area rn-section-gap">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <SectionTitle
-                  textAlign="text-center"
-                  radiusRounded=""
-                  subtitle="Previous Work"
-                  title="Check our recent works!"
-                  description="Experience our expertise through our portfolio."
-                />
-              </div>
+    <div className="main-content">
+      <div className="rwt-portfolio-area rn-section-gap">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <SectionTitle
+                textAlign="text-center"
+                radiusRounded=""
+                subtitle="Previous Work"
+                title="Check our recent works!"
+                description="Experience our expertise through our portfolio."
+              />
             </div>
-            <PortfolioOne
-              Column="col-lg-6 col-md-6 col-sm-12 col-12 mt--30 portfolio"
-              PortfolioData={data}
-            />
           </div>
+          <PortfolioOne
+            Column="col-lg-6 col-md-6 col-sm-12 col-12 mt--30 portfolio"
+            PortfolioData={data}
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
